2454fc7d090ad14693a3fd3fd717392baf719239
Petar Stojkovski <petars089@gmail.com>
2024-05-29 00:35:11 +0200

    feat(books): Added crud operations to the BookService and implemented inital logic

--- BookStore.Books/BookService.cs
+++ BookStore.Books/BookService.cs
@@ -1,4 +1,5 @@
-﻿namespace Bookstore.Books;
+﻿
+namespace Bookstore.Books;
 
 internal class BookService : IBookService
 {
@@ -9,6 +10,36 @@ internal class BookService : IBookService
         _bookRepository = bookRepository;
     }
 
+    public async Task CreateBookAsync(BookDto newBook)
+    {
+        var book = new Book(newBook.Id, newBook.Title, newBook.Author, newBook.Price);
+
+        await _bookRepository.AddAsync(book);
+        await _bookRepository.SaveChangesAsync();
+    }
+
+    public async Task DeleteBookAsync(Guid id)
+    {
+        var bookToDelete = await _bookRepository.GetByIdAsync(id);
+
+        //TODO handle case when book not found
+
+        if (bookToDelete is not null) 
+        {
+            await _bookRepository.DeleteAsync(bookToDelete);
+            await _bookRepository.SaveChangesAsync();
+        }
+    }
+
+    public async Task<BookDto> GetBookByIdAsync(Guid id)
+    {
+        var book = await _bookRepository.GetByIdAsync(id);
+
+        //TODO handle case when book not found
+
+        return new BookDto(book!.Id, book.Title, book.Author, book.Price);
+    }
+
     public async Task<List<BookDto>> ListBooksAsync()
     {
         var books = (await _bookRepository.ListAllAsync())
@@ -16,5 +47,17 @@ internal class BookService : IBookService
             .ToList();
 
         return books;
+    }
+
+    public async Task UpdateBookPriceAsync(Guid bookId, decimal newPrice)
+    {
+        //validate the price
+
+        var book = await _bookRepository.GetByIdAsync(bookId);
+
+        //TODO handle case when book not found
+
+        book!.UpdatePrice(newPrice);
+        await _bookRepository.SaveChangesAsync();
     }
 }
--- BookStore.Books/IBookService.cs
+++ BookStore.Books/IBookService.cs
@@ -3,4 +3,8 @@
 internal interface IBookService
 {
     Task<List<BookDto>> ListBooksAsync();
+    Task<BookDto> GetBookByIdAsync(Guid id);
+    Task CreateBookAsync(BookDto newBook);
+    Task DeleteBookAsync(Guid id);
+    Task UpdateBookPriceAsync(Guid bookId, decimal newPrice);
 }
