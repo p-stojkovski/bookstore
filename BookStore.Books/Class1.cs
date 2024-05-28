b75f82738852128a8338ff918eaa7b5bef00b63e
Petar Stojkovski <petars089@gmail.com>
2024-05-28 17:02:37 +0200

    feature(BooksStore): Create inital modular monolith playground  project with Books module

---
+++ BookStore.Books/Class1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Bookstore.Books;
+
+public interface IBookService
+{
+    IEnumerable<BookDto> ListBooks();
+}
+
+public record BookDto(Guid Id, string Title, string Author);
+
+public class BookService : IBookService
+{
+    public IEnumerable<BookDto> ListBooks()
+    {
+        return [new BookDto(Guid.NewGuid(), "The Fellowship of the Ring", "J.R.R. Tolkien")];
+    }
+}
+
+public static class BookEndpoints
+{
+    public static void MapBookEndpoints(this WebApplication app)
+    {
+        app.MapGet("/books", (IBookService bookService) =>
+        {
+            return bookService.ListBooks();
+        });
+    }
+}
