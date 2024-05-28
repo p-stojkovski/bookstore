9c7c25f952fb7151aba21dcd743df6eae7281b0a
Petar Stojkovski <petars089@gmail.com>
2024-05-29 00:14:03 +0200

    feat(books): Added initial Book entity

--- BookStore.Books/BookDto.cs
+++ BookStore.Books/BookDto.cs
@@ -1,3 +1,26 @@
-﻿namespace Bookstore.Books;
+﻿using Ardalis.GuardClauses;
+
+namespace Bookstore.Books;
 
 public record BookDto(Guid Id, string Title, string Author);
+
+internal class Book
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Title { get; private set; } = string.Empty;
+    public string Author { get; private set; } = string.Empty;
+    public decimal Price { get; private set; }
+
+    internal Book(Guid id, string title, string author, decimal price)
+    {
+        Id = Guard.Against.Default(id);
+        Title = Guard.Against.NullOrEmpty(title);
+        Author = Guard.Against.NullOrEmpty(author);
+        Price = Guard.Against.Negative(price);
+    }
+
+    internal void UpdatePrice(decimal price)
+    {
+        Price = Guard.Against.Negative(price);
+    }
+}
