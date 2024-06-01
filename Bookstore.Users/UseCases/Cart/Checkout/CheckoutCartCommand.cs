8bc4b175b55a9f8e102ea73a31557aca9dc21647
Petar Stojkovski <petars089@gmail.com>
2024-06-01 10:34:29 +0200

    feat(users): Separated command and handlers

--- Bookstore.Users/UseCases/Cart/Checkout/CheckoutCartCommand.cs
+++ Bookstore.Users/UseCases/Cart/Checkout/CheckoutCartCommand.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Ardalis.Result;
 using Bookstore.Books.Contracts;
-using Bookstore.OrderProcessing.Contracts;
 using Bookstore.Users.UseCases.Cart.AddItem;
 using MediatR;
 
@@ -14,46 +13,3 @@ namespace Bookstore.Users.UseCases.Cart.Checkout;
 internal record CheckoutCartCommand(string EmailAddress,
     Guid ShippingAddressId,
     Guid BillingAddressId) : IRequest<Result<Guid>>;
-
-internal class CheckoutCartCommandHandler : IRequestHandler<CheckoutCartCommand, Result<Guid>>
-{
-    private readonly IApplicationUserRepository _userRepository;
-    private readonly IMediator _mediator;
-
-    public CheckoutCartCommandHandler(IApplicationUserRepository userRepository, IMediator mediator)
-    {
-        _userRepository = userRepository;
-        _mediator = mediator;
-    }
-
-    public async Task<Result<Guid>> Handle(CheckoutCartCommand request, CancellationToken cancellationToken)
-    {
-        var user = await _userRepository.GetUserWithCartByEmailAsync(request.EmailAddress);
-        if (user is null)
-        {
-            return Result.Unauthorized();
-        }
-
-        var cartItems = user.CartItems
-            .Select(x => new OrderItemDetails(x.BookId, x.Quantity, x.UnitPrice, x.Description))
-            .ToList();
-
-        var createOrderCommand = new CreateOrderCommand(Guid.Parse(user.Id),
-            request.ShippingAddressId,
-            request.BillingAddressId,
-            cartItems);
-
-        //TODO: Consider replacing with a message-based approach for perf reasons
-        var result = await _mediator.Send(createOrderCommand, cancellationToken);
-        if (!result.IsSuccess)
-        {
-            return result.Map(x => x.OrderId);
-        }
-
-        user.ClearCart();
-
-        await _userRepository.SaveChangesAsync();
-
-        return Result.Success(result.Value.OrderId);
-    }
-}
---
+++ Bookstore.Users/UseCases/Cart/Checkout/CheckoutCartCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Ardalis.Result;
+using Bookstore.OrderProcessing.Contracts;
+using MediatR;
+
+namespace Bookstore.Users.UseCases.Cart.Checkout;
+
+internal class CheckoutCartCommandHandler : IRequestHandler<CheckoutCartCommand, Result<Guid>>
+{
+    private readonly IApplicationUserRepository _userRepository;
+    private readonly IMediator _mediator;
+
+    public CheckoutCartCommandHandler(IApplicationUserRepository userRepository, IMediator mediator)
+    {
+        _userRepository = userRepository;
+        _mediator = mediator;
+    }
+
+    public async Task<Result<Guid>> Handle(CheckoutCartCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetUserWithCartByEmailAsync(request.EmailAddress);
+        if (user is null)
+        {
+            return Result.Unauthorized();
+        }
+
+        var cartItems = user.CartItems
+            .Select(x => new OrderItemDetails(x.BookId, x.Quantity, x.UnitPrice, x.Description))
+            .ToList();
+
+        var createOrderCommand = new CreateOrderCommand(Guid.Parse(user.Id),
+            request.ShippingAddressId,
+            request.BillingAddressId,
+            cartItems);
+
+        //TODO: Consider replacing with a message-based approach for perf reasons
+        var result = await _mediator.Send(createOrderCommand, cancellationToken);
+        if (!result.IsSuccess)
+        {
+            return result.Map(x => x.OrderId);
+        }
+
+        user.ClearCart();
+
+        await _userRepository.SaveChangesAsync();
+
+        return Result.Success(result.Value.OrderId);
+    }
+}
