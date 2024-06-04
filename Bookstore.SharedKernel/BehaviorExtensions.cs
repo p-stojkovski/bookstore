cbd123a25edbfede6626d7104901c3bf6a809fb8
Petar Stojkovski <petars089@gmail.com>
2024-06-05 00:01:08 +0200

    feat(sharedKernel): Added Validation behavior pipeline

--- Bookstore.SharedKernel/BehaviorExtensions.cs
+++ Bookstore.SharedKernel/BehaviorExtensions.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using System.Reflection;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookstore.SharedKernel;
@@ -9,6 +11,42 @@ public static class BehaviorExtensions
     {
         services.AddScoped(typeof(IPipelineBehavior<,>),
             typeof(LoggingBehavior<,>));
+
+        return services;
+    }
+
+    public static IServiceCollection AddMediatRValidationBehavior(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+
+    public static IServiceCollection AddValidatorsFromAssemblyContaining<T>(this IServiceCollection services)
+    {
+        // Get the assembly containing the specified type
+        var assembly = typeof(T).GetTypeInfo().Assembly;
+
+        // Find all validator types in the assembly
+        var validatorTypes = assembly.GetTypes()
+            .Where(t => t.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                          i.GetGenericTypeDefinition() == typeof(IValidator<>)))
+            .ToList();
+
+        // Register each validator with its implemented interfaces
+        foreach (var validatorType in validatorTypes)
+        {
+            var implementedInterfaces = validatorType.GetInterfaces()
+                .Where(i => i.IsGenericType &&
+                          i.GetGenericTypeDefinition() == typeof(IValidator<>));
+
+            foreach (var implementedInterface in implementedInterfaces)
+            {
+                services.AddTransient(implementedInterface, validatorType);
+            }
+        }
 
         return services;
     }
--- Bookstore.SharedKernel/IntegrationEventBase.cs
+++ Bookstore.SharedKernel/IntegrationEventBase.cs
@@ -1,58 +1,8 @@
-﻿using System.Diagnostics;
-using System.Reflection;
-using Ardalis.GuardClauses;
-using MediatR;
-using Microsoft.Extensions.Logging;
+﻿using MediatR;
 
 namespace Bookstore.SharedKernel;
 
 public abstract record IntegrationEventBase : INotification
 {
     public DateTimeOffset DateTimeOffset { get; set; } = DateTimeOffset.UtcNow;
-}
-
-public class LoggingBehavior<TRequest, TResponse> :
-    IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
-{
-    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
-
-    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
-    {
-        _logger = logger;
-    }
-
-    public async Task<TResponse> Handle(TRequest request,
-        RequestHandlerDelegate<TResponse> next,
-        CancellationToken cancellationToken)
-    {
-        Guard.Against.Null(request);
-
-        if (_logger.IsEnabled(LogLevel.Information))
-        {
-            _logger.LogInformation("Handling {RequestName}", typeof(TRequest).Name);
-
-            Type myType = request.GetType();
-            IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
-            foreach (PropertyInfo prop in props)
-            {
-                object? propValue = prop.GetValue(request, null);
-                _logger.LogInformation("Property {Property} : {@Value}", prop.Name, propValue);
-            }
-        }
-
-        var sw = Stopwatch.StartNew();
-
-        var response = await next();
-
-        _logger.LogInformation("Handled {RequestName} with {Response} in {ms} ms", 
-            typeof(TRequest).Name,
-            response, 
-            sw.ElapsedMilliseconds);
-
-        sw.Stop();
-
-        return response;
-    }
-
-
 }
