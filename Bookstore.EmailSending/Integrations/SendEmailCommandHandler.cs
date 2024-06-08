97804b9ca7736073cb502e41519e2253dfda9bb8
Petar Stojkovski <petars089@gmail.com>
2024-06-08 09:15:10 +0200

    feat(emailSending): Added integration handler - SendEmailCommandHandler

---
+++ Bookstore.EmailSending/Integrations/SendEmailCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Ardalis.Result;
+using MediatR;
+
+namespace Bookstore.EmailSending.Integrations;
+
+internal class SendEmailCommandHandler : IRequestHandler<SendEmailCommand, Result<Guid>>
+{
+    private readonly ISendEmail _emailSender;
+
+    public SendEmailCommandHandler(ISendEmail emailSender)
+    {
+        _emailSender = emailSender;
+    }
+
+    public async Task<Result<Guid>> Handle(SendEmailCommand request, CancellationToken cancellationToken)
+    {
+        await _emailSender.SendEmailAsync(request.To,
+            request.From,
+            request.Subject,
+            request.Body);
+
+        return Guid.Empty;
+    }
+}
