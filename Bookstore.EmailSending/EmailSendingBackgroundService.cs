955e8fea96b64cdd08b4c3ffbd9d7b57d360a5ab
Petar Stojkovski <petars089@gmail.com>
2024-06-12 19:59:27 +0200

    feat(emailSending): Added initial EmailSendingBackgroundService logic

---
+++ Bookstore.EmailSending/EmailSendingBackgroundService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Bookstore.EmailSending;
+
+public class EmailSendingBackgroundService : BackgroundService
+{
+    private readonly ILogger<EmailSendingBackgroundService> _logger;
+
+    public EmailSendingBackgroundService(ILogger<EmailSendingBackgroundService> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        int delayMilliseconds = 10_000; // 10 seconds
+
+        _logger.LogInformation("{serviceName} starting...", nameof(EmailSendingBackgroundService));
+
+        while(!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex) 
+            {
+                _logger.LogError("Error processing outbox: {message}", ex.Message);
+            }
+            finally
+            {
+                await Task.Delay(delayMilliseconds);
+            }
+        }
+
+        _logger.LogInformation("{serviceName} stopping.", nameof(EmailSendingBackgroundService));
+    }
+}
