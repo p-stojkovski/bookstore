00fbe359412d651f61c936865d9e2a1e46f80cae
Petar Stojkovski <petars089@gmail.com>
2024-06-13 23:19:40 +0200

    feat(reporting): Added inital api endpoint for getting TopSalesByMonth

---
+++ Bookstore.Reporting/ReportEndpoints/TopSalesByMonth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+
+namespace Bookstore.Reporting.ReportEndpoints;
+
+internal record TopSalesByMonthRequest(int Year, int Month);
+internal record TopSalesByMonthResponse();
+
+internal class TopSalesByMonth : Endpoint<TopSalesByMonthRequest, TopSalesByMonthResponse>
+{
+    public override void Configure()
+    {
+        Get("/topsales");
+        AllowAnonymous(); //TODO: lock down
+    }
+
+    public override async Task HandleAsync(TopSalesByMonthRequest request, 
+        CancellationToken cancellationToken = default)
+    {
+        var response = new TopSalesByMonthResponse();
+
+        await SendAsync(response);
+    }
+}
