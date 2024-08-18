fa6016e8777d817ab2318c426772d5609900a6aa
Petar Stojkovski <petars089@gmail.com>
2024-08-18 10:45:31 +0200

    feat(EmailSending): added QueueEmailsInOutboxService

---
+++ Bookstore.EmailSending/QueueEmailsInOutboxService.cs
@@ -0,0 +1,20 @@
+﻿using Ardalis.Result;
+using Bookstore.EmailSending.Integrations;
+using MongoDB.Driver;
+
+namespace Bookstore.EmailSending;
+
+internal class QueueEmailsInOutboxService : IQueueEmailsInOutboxService
+{
+    private readonly IMongoCollection<EmailOutboxEntity> _emailCollection;
+
+    public QueueEmailsInOutboxService(IMongoCollection<EmailOutboxEntity> emailCollection)
+    {
+        _emailCollection = emailCollection;
+    }
+
+    public async Task QueueEmailForSendingAsync(EmailOutboxEntity entity)
+    {
+        await _emailCollection.InsertOneAsync(entity);
+    }
+}
